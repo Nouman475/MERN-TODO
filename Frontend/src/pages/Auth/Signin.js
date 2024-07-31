d3a7e5c4475c353e48413ca84f8339c4c8b2e7f5
NoumanBunty <rn9728959@gmail.com>
2024-07-31 16:18:09 +0500

    debug

--- Frontend/src/pages/Auth/Signin.js
+++ Frontend/src/pages/Auth/Signin.js
@@ -49,8 +49,8 @@ export default function Login() {
           notify("Login success","success")
         }
       })
-      .catch(() => {
-        notify("NETWORK ANOMALY","error")
+      .catch((err) => {
+        console.log(err)
       });
   };
 
